384ea2bf65515e49cda2542a3a6ede1d199c27e7
Kyle Par <shuttle876@gmail.com>
2025-05-21 20:50:28 -0400

    added products to add products in db

--- project2/backend/addProducts.py
+++ project2/backend/addProducts.py
@@ -16,7 +16,10 @@ def add_product(name, description, price, stock, category_id, image_url, feature
     print("Product added successfully!")
 
 # Example Usage
-add_product("Gaming Laptop", "High-end gaming laptop", 1299.99, 10, 1, "images/laptop.jpg", True)
+add_product("16in HP high end gaming Laptop", " 16 in high end gaming laptop", 1599.99, 120, 1, 
+            "https://www.bing.com/images/search?view=detailV2&ccid=ZA7d7MoD&id=D54778CF9215536413B3C94088998A3724E6AF6D&thid=OIP.ZA7d7MoDZnMe4ao84yo53gHaFJ&mediaurl=https%3a%2f%2fcdn.oneesports.id%2fcdn-data%2fwp-content%2fuploads%2f2019%2f07%2fHPpavilionultraviolet.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.640eddecca0366731ee1aa3ce32a39de%3frik%3dba%252fmJDeKmYhAyQ%26pid%3dImgRaw%26r%3d0&exph=556&expw=800&q=hp+high+end+gaming+laptop&simid=608001326772480485&FORM=IRPRST&ck=CFDF5CED88D0E5DFC33D926D79524DB9&selectedIndex=9&itb=0", 
+            True
+           )
 
 ### add products
 
@@ -31,7 +34,6 @@ get_products()
 def get_products_by_category(category_id):
     products = Product.query.filter_by(category_id=category_id).all()
     return products
-
 
 def update_product(product_id, new_price, new_stock):
     product = Product.query.get(product_id)
